4405e9418a4bccc2064fd59f1ebaf545031a4824
daniel <silva_120000@yahoo.com.br>
2018-06-19 00:33:08 -0300

    arrumado o erro  ao criar o autoload

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$sql = new Hcode\db\Sql();
+	$sql = new Hcode\DB\Sql();
 	$results =  $sql->select("SELECT * FROM tb_users");
 	echo json_encode($results);
 
